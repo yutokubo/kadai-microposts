2b334cbdcabb0abda0e6f6ad110fc17fb5d728db
yuto@ <samson200761@gmail.com>
2021-11-02 09:21:17 +0000

    follow, unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,16 +20,102 @@ class UsersController extends Controller
     }
     
         
-    public function show($id)
+        public function show($id)
     {
         // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
 
-        // ユーザ詳細ビューでそれを表示
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        // ユーザ詳細ビューでそれらを表示
         return view('users.show', [
             'user' => $user,
+            'microposts' => $microposts,
         ]);
     }
     
+     /**
+     * ユーザをフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function store($id)
+    {
+        // 認証済みユーザ（閲覧者）が、 idのユーザをフォローする
+        \Auth::user()->follow($id);
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
+
+    /**
+     * ユーザをアンフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // 認証済みユーザ（閲覧者）が、 idのユーザをアンフォローする
+        \Auth::user()->unfollow($id);
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
+    
+    // 中略
+
+    /**
+     * ユーザのフォロー一覧ページを表示するアクション。
+     *
+     * @param  $id  ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function followings($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロー一覧を取得
+        $followings = $user->followings()->paginate(10);
+
+        // フォロー一覧ビューでそれらを表示
+        return view('users.followings', [
+            'user' => $user,
+            'users' => $followings,
+        ]);
+    }
+
+    /**
+     * ユーザのフォロワー一覧ページを表示するアクション。
+     *
+     * @param  $id  ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function followers($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロワー一覧を取得
+        $followers = $user->followers()->paginate(10);
+
+        // フォロワー一覧ビューでそれらを表示
+        return view('users.followers', [
+            'user' => $user,
+            'users' => $followers,
+        ]);
+    }
+}
+
     
 }
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -14,7 +14,6 @@
                         {{-- 投稿内容 --}}
                         <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                     </div>
-                    
                     <div>
                         @if (Auth::id() == $micropost->user_id)
                             {{-- 投稿削除ボタンのフォーム --}}
